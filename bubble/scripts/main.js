3d252ac3dd8c141473be628889bb19168a25ec25
SchlagerAndreas <andreas.schlager49@gmail.com>
2021-04-11 15:44:31 +0200

    improved background bubble animation

--- bubble/scripts/main.js
+++ bubble/scripts/main.js
@@ -5,63 +5,77 @@ window.onload = function(){
 class BubbleManager{
     constructor(){
         this.setupBackgroundBubbles();
+        window.onresize = () => this.setupBackgroundBubbles();
+
         let width = screen.width < 1000 ? screen.width : 1000
-        document.getElementById("bubbleWrapper").setAttribute("style","height:" + 2*width + "px");
-        var app = new PIXI.Application(
+        document.getElementById("bubbleWrapper").setAttribute("style","height:" + 2*width + "px");   
+        this.app = new PIXI.Application(
             {
                 width: width,
                 height: 2 * width,
                 transparent: true,
             }
         );
-        document.querySelector("#pixiCanvas").appendChild(app.view);
+        document.querySelector("#pixiCanvas").appendChild(this.app.view);
     
-        var bubbles = [];
+        this.bubbles = [];
         //Projects:
-        bubbles.push(new Bubble("Enigma",
+        let bubbleRadius = 120;
+        this.bubbles.push(new Bubble("Enigma",
                                 "An online \n Enigma Machine",
                                 {webpage:"https://schlagerandreas.github.io/enigma/",github:"https://github.com/SchlagerAndreas/enigma"},
                                 "t",
-                                120,
-                                Math.random() * app.screen.width,
-                                1740));
-        app.stage.addChild(bubbles[0])
+                                bubbleRadius,
+                                0,
+                                0));
+        this.app.stage.addChild(this.bubbles[0])
     
-        bubbles.push(new Bubble("Mazertron 3000",
+        this.bubbles.push(new Bubble("Mazertron 3000",
                                 "A webpage where you can \n generate and solve mazes.",
                                 {webpage:"https://schlagerandreas.github.io/maze-generator/",github:"https://github.com/SchlagerAndreas/maze-generator"},
                                 "t",
-                                120,
-                                Math.random() * app.screen.width,
-                                1740));
-        app.stage.addChild(bubbles[1])
+                                bubbleRadius,
+                                this.app.screen.width / 2 - bubbleRadius,
+                                1 * bubbleRadius));
+        this.app.stage.addChild(this.bubbles[1])
     
-        bubbles.push(new Bubble("Robots",
+        this.bubbles.push(new Bubble("Robots",
                                 "An online mini Game. \n Play a soldier and  \n fight against waves of Robots",
                                 {webpage:"https://schlagerandreas.github.io/Robot/",github:"https://github.com/SchlagerAndreas/Robot"},
                                 "t",
-                                120,
-                                Math.random() * app.screen.width,
-                                1740));
-        app.stage.addChild(bubbles[2])
+                                bubbleRadius,
+                                this.app.screen.width - 2 * bubbleRadius,
+                                2 * bubbleRadius));
+        this.app.stage.addChild(this.bubbles[2])
     
-        bubbles.push(new Bubble("Rasputin",
+        this.bubbles.push(new Bubble("Rasputin",
                                 "-",
                                 {github:"https://github.com/SchlagerAndreas"},
                                 "t",
-                                120,
-                                Math.random() * app.screen.width,
-                                1740));
-        app.stage.addChild(bubbles[3]);
+                                bubbleRadius,
+                                this.app.screen.width / 2,
+                                3 * bubbleRadius));
+        this.app.stage.addChild(this.bubbles[3]);
+        this.app.ticker.add(()=>this.update())
     }
 
     setupBackgroundBubbles(){
         let bubbleWrapper = document.getElementById("bubbleWrapper");
-        for(let i = 0; i < Math.round(bubbleWrapper.offsetWidth / 10); i++){
+        while(bubbleWrapper.firstChild){
+            bubbleWrapper.removeChild(bubbleWrapper.firstChild); 
+        }
+
+        for(let i = 0; i < Math.round(bubbleWrapper.offsetWidth / 5); i++){
             let div = document.createElement("div");
             div.setAttribute("class","bgBubble")
             div.setAttribute("style","left:" + Math.round(Math.random() * (bubbleWrapper.offsetWidth - 20)) + "px; animation: moveUp " + ((Math.random() * 10) + 5) + "s linear infinite");
             bubbleWrapper.appendChild(div)
+        }
+    }
+
+    update(){
+        for(let i = 0; i < this.bubbles.length; i++){
+            this.bubbles[i].update(this.app);
         }
     }
 }
